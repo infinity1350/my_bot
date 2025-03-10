0280393b7e45b99cb6377ca79526cede055a85c9
inifinity1350 <daisonedwin@gmail.com>
2025-03-10 10:49:00 +0530

    Refactor launch_sim.launch.py to improve structure and fix argument names

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,44 +1,43 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArguments
+from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 
 from launch_ros.actions import Node
 
-def generate_launch_descriptions():
+def generate_launch_description():
 
-    package_name = 'robot_testing'
+    package_name = 'my_bot'
 
 
     #include the launch file rsp.launch.py
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
+                PythonLaunchDescriptionSource(os.path.join(
                     get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-                )]), launch_arguements = {'use_sim_time': 'true'}.items()
+                )), launch_arguments = {'use_sim_time': 'true'}.items()
 
     )
 
     #include the gazebo launch file
 
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
+                PythonLaunchDescriptionSource(os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-                )])
+                ))
                 
     )
 
     #include the spwan robot launch file
     spawn_robot = Node(package = 'ros_gz_sim', executable = 'create', 
-                        arguements = ['-topic', 'robot_desciption',
+                        arguments = ['-topic', 'robot_description',
                                       '-entity', 'robot'],
                         output = 'screen'
     )
 
-    return IncludeLaunchDescription(
-        [rsp, 
-         gazebo,
-         spawn_robot
-         ])
+    ld = LaunchDescription()
+    ld.add_action(rsp)
+    ld.add_action(gazebo)
+    ld.add_action(spawn_robot)
+    return ld
