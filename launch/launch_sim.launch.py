951fe6122b95bb3c78e7f34a57451edd1652b79a
inifinity1350 <daisonedwin@gmail.com>
2025-03-18 18:03:05 +0530

    Fix typos in robot_control.xarco, enhance material definitions in robot_core.xacro, and update launch_sim.launch.py for improved simulation parameters

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -6,9 +6,11 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
 
     package_name = 'my_bot'
+    print(os.path.join(get_package_share_directory(package_name)))
 
 
     #include the launch file rsp.launch.py
@@ -23,16 +25,14 @@ def generate_launch_description():
     default_world = os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world')
     
     #declare the world argument
-    world = LaunchConfiguration('world')
-
     world_arg = DeclareLaunchArgument('world', default_value=default_world, description='Loading World file')
-
+    world = LaunchConfiguration('world')
 
     #include the gazebo launch file
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-                )), launch_arguments = {'gz_args': ['-r -v4', world], 'on_exit_shutdown':'true'}.items()
+                )), launch_arguments = {'gz_args': ['-r -v 4 ', world], 'on_exit_shutdown':'true'}.items()
                 
     )
 
@@ -40,13 +40,13 @@ def generate_launch_description():
     spawn_robot = Node(package = 'ros_gz_sim', executable = 'create', 
                         arguments = ['-topic', 'robot_description',
                                       '-name', 'my_bot',
-                                      '-z', '0.1',],
+                                      'z', '-5.0'],
                         output = 'screen'
     )
 
     ld = LaunchDescription()
+    ld.add_action(world_arg)
     ld.add_action(rsp)
     ld.add_action(gazebo)
     ld.add_action(spawn_robot)
-    ld.add_action(world_arg)
     return ld
