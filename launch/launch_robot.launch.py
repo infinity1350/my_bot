17c175967eb2188cc48f28509ac9f358581e9621
inifinity1350 <daisonedwin@gmail.com>
2025-04-23 19:20:43 +0530

    Update robot configuration: change SLAM mode to mapping, remove my_controllers.yaml, add robot_controllers.yaml, and enhance launch file for controller management

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -2,16 +2,20 @@ import os
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
 
     package_name = 'my_bot'
-    print(os.path.join(get_package_share_directory(package_name)))
-
+    
+    robot_controllers = os.path.join(
+        get_package_share_directory(package_name), 'config', 'robot_controllers.yaml'
+    )
+    
 
     #include the launch file rsp.launch.py
     rsp = IncludeLaunchDescription(
@@ -21,34 +25,52 @@ def generate_launch_description():
 
     )
 
-    controller_manager = Node(
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_node = Node(
         package = 'controller_manager',
-        executable = 'spawner.py,',
-        parameter = [os.join(get_package_share_directory(package_name),'config','controllers.yaml')],
-        output = 'screen'
+        executable = 'ros2_control_node',
+        parameters = [{'robot_description':robot_description},robot_controllers],
+        output = 'both'
     )
 
-    diff_drive_spawner = Node(
-        package = 'controller_manager'
-        executable = spwaner.py,
-        arguements  = ['diff_drive_controller', '--controller-manager', 'controller_manager'],
-        output = 'screen'
+    robot_pub_sub = Node(
+        package = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        output = 'both',
+        parameters = [robot_description],
+
     )
 
-    joint_state_broadcaster = Node()
+    joint_state_broadcaster_spawner = Node(
+        package = 'controller_manager',
+        executable = 'spawner',
+        arguments= ['joint_state_broadcaster'],
+    )
 
-    serial_node = Node(
-        package = 'my_bot'
-        executable = 'serial_bridge',
-        parameters = [{'port' : '/dev/ttyUSB0', 'baud_rate' : 115200}],
-        output = 'screen'
+    robot_controller_spawner = Node(
+        package = 'controller_manager',
+        executable = 'spawner',
+        arguments = ['diffbot_base_controller',
+                      '--param-file',
+                      robot_controllers,
+                      '--controller-ros-args',
+                      '-r /diffbot_base_controller/cmd_vel:=/cmd_vel',],
+    )
+
+    delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_controller_spawner,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
     )
  
     ld = LaunchDescription()
  
     ld.add_action(rsp)
-    ld.add_action(controller_manager)
-    ld.add_action(diff_drive_spawner)
-    ld.add_action(serial_node)
-  
+    ld.add_action(controller_node)
+    ld.add_action(robot_pub_sub)
+    ld.add_action(delay_joint_state_broadcaster_after_robot_controller_spawner)
+    ld.add_action(robot_controller_spawner)
+   
     return ld
